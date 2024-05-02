80dacb51922232b94928e357fd2ade5ad91bf2fc
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-05-02 14:44:47 +0200

    commit

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = ({ element, isLoggedIn }) => {
   // Si está autorizado, renderiza el contenido del Outlet
