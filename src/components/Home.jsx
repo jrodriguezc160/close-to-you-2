60f9a5b37f588e2bc1b845cfd9398f04ef3b0b03
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-05-13 10:42:33 +0200

    home structure

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from 'react';
-import { followUsuario, unfollowUsuario, searchUsuarios, getUsuariosSeguidos } from '../services/UserServices';
+import { followUsuario, unfollowUsuario, getUsuariosSeguidos } from '../services/UserServices';
+import '../styles/home.css';
 
 const Home = ({ currentUser }) => {
   const [responseData, setResponseData] = useState([]);
-  const [isFollowed, setIsFollowed] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const usuarios = await searchUsuarios('') // Llama a la función searchUsuarios con el término de búsqueda
-        const formattedData = formatUsuariosData(usuarios); // Formatea los datos de los usuarios
-        setResponseData(formattedData); // Establece los datos formateados en el estado
+        const usuarios = await getUsuariosSeguidos(currentUser);
+        const formattedData = formatUsuariosData(usuarios);
+
+        setResponseData(formattedData.map(user => ({ ...user, isFollowed: true }))); // Inicializa el estado de seguimiento para cada usuario como falso
+        console.log('Primer elemento de reponseData', responseData[2])
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -35,28 +37,28 @@ const Home = ({ currentUser }) => {
     }));
   };
 
-  // Seguir y dejar de seguir usuarios
-  const handleFollowUser = async (targetUser) => {
-    if (!isFollowed) {
-      try {
-        await followUsuario(targetUser.id, currentUser);
-        console.log('Usuario seguido con éxito');
-        await getUsuariosSeguidos();
-        setIsFollowed(true)
-      } catch (error) {
-        console.log(targetUser.image)
-        console.error('Error al seguir al usuario');
-      }
-    } else {
-      console.warn('Ya se sigue a este usuario');
+  const handleFollowUser = async (targetUserIndex) => {
+    try {
+      const targetUser = responseData[targetUserIndex];
+      await followUsuario(targetUser.id, currentUser);
+      await getUsuariosSeguidos();
+      const updatedResponseData = [...responseData];
+      updatedResponseData[targetUserIndex] = { ...targetUser, isFollowed: true };
+      setResponseData(updatedResponseData);
+      console.log('Usuario seguido con éxito');
+    } catch (error) {
+      console.error('Error al seguir al usuario');
     }
   }
 
-  const handleUnfollowUser = async (targetUser) => {
+  const handleUnfollowUser = async (targetUserIndex) => {
     try {
-      await unfollowUsuario(targetUser.id, currentUser)
+      const targetUser = responseData[targetUserIndex];
+      await unfollowUsuario(targetUser.id, currentUser);
       await getUsuariosSeguidos();
-      setIsFollowed(false)
+      const updatedResponseData = [...responseData];
+      updatedResponseData[targetUserIndex] = { ...targetUser, isFollowed: false };
+      setResponseData(updatedResponseData);
       console.log('Usuario dejado de seguir con éxito');
     } catch (error) {
       console.error('Error al dejar de seguir al usuario');
@@ -65,41 +67,40 @@ const Home = ({ currentUser }) => {
 
   return (
     <div className="two-columns">
-      <div className="left-column">
-        <div className="results-column">
-          {responseData.map((user) => {
-            return (
-              <div className='result'>
-                <div className={`result-pic users-result`}>
-                  <img src={user.image} alt="result-pic" />
-                </div>
+      <div className="left-column" style={{ justifyContent: 'flex-end', gap: '1rem' }}>
+        {responseData.map((user, index) => {
+          return (
+            <div className='search-result' key={user.id} style={{ backgroundColor: 'var(--semi-transparent)' }}>
+              <div className="info">
                 <div className="result-text">
+                  <div className='result-pic users-result' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <img src={user.image} alt="result-pic" />
+                  </div>
                   <div className="user-result-text">
                     <b>{user.title}</b>&nbsp;&nbsp;@{user.authors}
                   </div>
+                </div>
 
-                  <div className="result-buttons">
-                    <div style={{ display: 'flex', alignItems: 'center', gap: '.5rem' }}>
-                      {/* Si el usuario actual no siga al usuario mostrado... */}
-
-                      {currentUser !== parseInt(user.id) && isFollowed && (
-                        <div className='nav-button no-text selected' onClick={handleUnfollowUser(user)}><i data-feather="user-check"></i><span>Seguido</span></div>
-                      )}
-                      {currentUser !== parseInt(user.id) && !isFollowed && (
-                        <div className='nav-button no-text' onClick={handleFollowUser(user)}><i data-feather="user-plus"></i><span>Seguir</span></div>
-                      )}
-                      <div className='nav-button no-text' /* onClick={() => handleVerPerfil(user.id)} */><i data-feather="external-link"></i><span>Ver perfil</span></div>
-                    </div>
+                <div className="result-buttons">
+                  <div style={{ display: 'flex', alignItems: 'center', gap: '.5rem' }}>
+                    {currentUser !== parseInt(user.id) && user.isFollowed && (
+                      <div className='nav-button no-text selected' onClick={() => handleUnfollowUser(index)}><i data-feather="user-check"></i></div>
+                    )}
+                    {currentUser !== parseInt(user.id) && !user.isFollowed && (
+                      <div className='nav-button no-text' onClick={() => handleFollowUser(index)}><i data-feather="user-plus"></i></div>
+                    )}
+                    <div className='nav-button no-text'><i data-feather="external-link"></i></div>
                   </div>
                 </div>
               </div>
-            )
-          })}
-        </div>
+
+              <div className="nav-button no-text arrow-right"><i data-feather='arrow-right'></i></div>
+            </div>
+          )
+        })}
       </div>
-      <div className="right-column">
-      </div>
-    </div >
+      <div className="right-column"></div>
+    </div>
   )
 }
 
