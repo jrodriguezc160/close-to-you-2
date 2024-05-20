d96077194d75d2d30952b1f7457c36a83ec09bab
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-05-20 09:24:53 +0200

    changes

--- src/components/profilepage/PostShowcase.jsx
+++ src/components/profilepage/PostShowcase.jsx
@@ -67,25 +67,24 @@ const PostShowcase = ({ datosUsuario, userPosts, handleOpenCollections }) => {
                   </div>
                 </div>
                 <div className="post-elements">
-                  <div className="post-name">{datosUsuario?.nombre_mostrado}</div>
-                  <div className="post-username">@{datosUsuario?.usuario}</div>
-                  <div className="post-text">{post?.contenido}</div>
-                  <div className="post-content" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <div className="post-images" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                      <div className='post-image'>
-                        <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQg6exx0ZTbGvPwynge3k-uNPIaaai_sIb9pdRXfMDgqtEMqyQKK7gCuFsc1XhNEIFgq8m2qOJIQzSzzHn-1Z-tAdylYK-AjLoXV1wmkQtjvo-NKjBpx_cu8A&usqp=CAc" alt='dune' />
-                      </div>
-                      <div className='post-image'>
-                        <img src="https://m.media-amazon.com/images/I/718W0JbHm1L._AC_UF894,1000_QL80_.jpg" alt='normal people' />
-                      </div>
-                      <div className='post-image'>
-                        <img src="https://m.media-amazon.com/images/I/81fS9LRN29L._AC_UF894,1000_QL80_.jpg" alt='harry potter' />
-                      </div>
+                  <div className="post-top">
+                    <div className="post-username">
+                      <div><b>{datosUsuario?.nombre_mostrado}</b></div>
+                      <div>@{datosUsuario?.usuario}</div>
                     </div>
 
-                    <div className='see-more'>
-                      <div className="nav-button" onClick={() => handleOpenCollections('users', 99)}><i data-feather="package"></i>Ver colección</div>
-                      <div className="nav-button no-text"><i data-feather="maximize-2"></i></div>
+                    <div className="post-text">{post?.contenido}</div>
+                    <div className="buttons">
+                      <div className="nav-button no-text"><i data-feather="heart"></i></div>
+                      <div className="nav-button no-text"><i data-feather="repeat"></i></div>
+                      <div className="nav-button no-text"><i data-feather="message-circle"></i></div>
+                    </div>
+                  </div>
+
+                  <div className="post-bottom">
+                    <div className="comments">
+                      <span>Comentarios</span>
+                      <div className="comment-bar"></div>
                     </div>
                   </div>
                 </div>
