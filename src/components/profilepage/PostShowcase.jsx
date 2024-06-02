8647dcc51177e7d984457a1e54120c8bfb9c371a
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-06-02 22:34:25 +0200

    fechas

--- src/components/profilepage/PostShowcase.jsx
+++ src/components/profilepage/PostShowcase.jsx
@@ -1,6 +1,8 @@
 import '../../styles/posts.css'
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react';
 import { addLike, deleteLike, checkUserLike, addRepost, deleteRepost, checkUserRepost } from '../../services/PostServices'; // Importa los servicios necesarios
+import { format } from 'date-fns';
+import { es } from 'date-fns/locale';
 
 const PostShowcase = ({ datosUsuario, userPosts, currentUser, setShowPostsModal, setDeletePublicacionModal, setDeletePostId }) => {
   const [dotIndex, setDotIndex] = useState(4);
@@ -141,6 +143,27 @@ const PostShowcase = ({ datosUsuario, userPosts, currentUser, setShowPostsModal,
     <div className='posts-holder' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className="post-stack" ref={stackRef}>
         {userPosts.slice().reverse().map((post, index) => { // Invertir el array userPosts
+          // Calcula la fecha de la publicación
+          const postDate = new Date(post.fecha);
+          let formattedDate;
+
+          const now = new Date();
+          const diffInMinutes = Math.floor((now - postDate) / 1000 / 60);
+          const diffInHours = Math.floor(diffInMinutes / 60);
+          const diffInDays = Math.floor(diffInHours / 24);
+
+          if (diffInMinutes < 1) {
+            formattedDate = 'Justo ahora';
+          } else if (diffInMinutes < 60) {
+            formattedDate = `Hace ${diffInMinutes} minutos`;
+          } else if (diffInHours < 24) {
+            formattedDate = `Hace ${diffInHours} horas`;
+          } else if (diffInDays < 7) {
+            formattedDate = `Hace ${diffInDays} días`;
+          } else {
+            formattedDate = format(postDate, "d 'de' MMMM 'de' yyyy", { locale: es });
+          }
+
           return (
             <div className="post-showcase-grid" key={post.id} data-post-id={post.id}>
               <div className="post" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
@@ -168,19 +191,14 @@ const PostShowcase = ({ datosUsuario, userPosts, currentUser, setShowPostsModal,
                         {post.reposts}
                         <i data-feather="repeat"></i>
                       </div>
-                      <span style={{ color: 'var(--gray-2)' }}>·&nbsp;&nbsp;{post?.fecha}&nbsp;&nbsp;·</span>
-                      <div className="nav-button no-text interactive trash" onClick={() => handleDeleteClick(post.id)}>
-                        <i data-feather="trash"></i>
-                      </div>
+                      <span style={{ color: 'var(--gray-2)' }}>·&nbsp;&nbsp;{formattedDate}&nbsp;&nbsp;·</span>
+                      {currentUser === post.userId && (
+                        <div className="nav-button no-text interactive trash" onClick={() => handleDeleteClick(post.id)}>
+                          <i data-feather="trash"></i>
+                        </div>
+                      )}
                     </div>
                   </div>
-
-                  {/* <div className="post-bottom">
-                    <div className="comments">
-                      <span>Comentarios</span>
-                      <div className="comment-bar"></div>
-                    </div>
-                  </div> */}
                 </div>
               </div>
             </div>
